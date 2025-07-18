44ddb84b60fee3c1d4212bf703ded012a99df508
Tia Piol <tiarox02@yahoo.com>
2025-07-18 10:16:17 -0700

    connected to mongodb

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-// Here is where we import modules
-// We begin by loading Express
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require('express');
+const mongoose = require("mongoose");
 
 const app = express();
 
-// GET
 
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+
+
+
+// GET
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 })
