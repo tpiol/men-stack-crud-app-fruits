14d5ddcbb626a9890aea9802e4b64ffb68bf4fa2
Tia Piol <tiarox02@yahoo.com>
2025-07-18 09:47:31 -0700

    my landing page

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require('express');
 
 const app = express();
 
+// GET
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
